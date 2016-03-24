b3b69914d5c64890a06c21889b8cb1d537b0d224
Julian Verschuren <julianverschuren@hotmail.com>
2016-03-24 13:22:20 +0100

    klaar
    alles klaar

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var clickedTotal = timesClicked*Math.random()*5;
 var executeAmount = document.getElementById('amount'); //waarde van executeamount word uit de html gehaald en in de variabele gestopt.
 
 var executeRating = document.getElementById('rating');//waarde van executerating word uit de html gehaald en in de variabele gestopt.
+var theStarz = document.querySelectorAll('.stars');
 
 function filledStar(specificStar, percent){ //functie die een specifiek ster opvult
 
@@ -22,7 +23,9 @@ function filledStar(specificStar, percent){ //functie die een specifiek ster opv
 
 
 function fillingUpTheStars(rating){ //deze functie zorgt ervoor dat alle sterren worden gekleurd
-for (var i=1; i<6; i++){ //een for loop die door alle sterren heen gat
+for (var i=1; i<6; i++){ 
+
+//een for loop die door alle sterren heen gat
 filledStar(i, rating*100); //rating word hier keer 100 gedaan
 rating --; //hier word er wat vanaf gehaald dus als er lager gestemt word gaat de rating gemiddeld omlaag
 
@@ -36,7 +39,26 @@ fillingUpTheStars(0.3) //hier roep je de functie aan op alle sterren op te vulle
 
 
 // random genereren en uitvoeren in dom:
+function execute(){ //functie om het uit te voeren voor elke ster in plaat svan 1
+	executeAmount.innerHTML = timesClicked; //voert daadwerkelijk de aantalkeren geklikt uit
+	executeRating.innerHTML = Math.ceil(10*clickedTotal/timesClicked)/10; //voert daadwerkelijk de hele 		waardering uit, rondt af, en deelt door 10.
+	fillingUpTheStars(Math.ceil(10*clickedTotal/timesClicked)/10);
 
-executeAmount.innerHTML = timesClicked; //voert daadwerkelijk de aantalkeren geklikt uit
-executeRating.innerHTML = Math.ceil(10*clickedTotal/timesClicked)/10; //voert daadwerkelijk de hele waardering uit, rondt af, en deelt door 10.
-fillingUpTheStars(Math.ceil(10*clickedTotal/timesClicked)/10);
+}
+execute();
+
+	function processClick(){
+
+			timesClicked ++; //voegt een aantal keer geklikt toe
+			var numbber = this.getAttribute('data-role'); //pakt het atrribuut data-role uit de html en stopt het in de variabele numbber
+			clickedTotal += parseInt(numbber); //inhoud omgezet naar een heel getal doormiddel van parseInt
+			execute(); //roept de execute functie aan
+
+
+}
+
+for(var i=0; i<theStarz.length; i++){ //deze forloop zorgt ervoor dat elke keer als je op een ster klikt functie van processclick door gaat tot de sterren hun lengte bereikt is
+
+theStarz[i].addEventListener('click', processClick); //een onclick eventlistener voor de sterren.
+
+}
